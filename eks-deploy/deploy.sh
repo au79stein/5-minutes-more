de9bc559ab06c6b7082f8ec55d48454f4a9b1bf6
cloudnost <rich@cloudnost.com>
2025-04-12 13:23:07 -0400

    ebs-csi-sc

--- eks-deploy/deploy.sh
+++ eks-deploy/deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+kubectl create namespace postgres
+kubectl create namespace jenkins
+kubectl create namespace ingress
+#
+kubectl apply -f postgres/secret.yaml -n postgres
+kubectl apply -f jenkins/secret.yaml -n jenkins
+#
 helm upgrade --install postgres oci://registry-1.docker.io/bitnamicharts/postgresql -n postgres -f postgres/values.yaml
 helm upgrade --install jenkins oci://registry-1.docker.io/bitnamicharts/jenkins -n jenkins -f jenkins/values.yaml
 kubectl apply -f ingress/jenkins-ingress.yaml
